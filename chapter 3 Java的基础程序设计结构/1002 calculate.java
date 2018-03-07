3a88f3f70780977cb7018562feccab0b274c9108
maximusyoung007 <1091261998@qq.com>
2018-03-07 22:10:27 +0800

    chapter 3

---
+++ chapter 3 Java的基础程序设计结构/1002 calculate.java
@@ -0,0 +1,8 @@
+public class calculate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.print(2.0 - 1.1);//because binary system can't represent 1/10 precisely
+	}
+
+}
