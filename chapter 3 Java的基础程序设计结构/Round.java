43d47fc20f13b45089f99d91dc64d5d3967683f5
maximusyoung007 <1091261998@qq.com>
2018-03-20 19:21:07 +0800

    chapter3

---
+++ chapter 3 Java的基础程序设计结构/Round.java
@@ -0,0 +1,11 @@
+import static java.lang.Math.*;
+public class Round {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 9.99;
+		int b = (int)round(a);
+		System.out.println(b);
+	}
+
+}
