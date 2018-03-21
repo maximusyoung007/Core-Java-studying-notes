0be74943c999af2c09aad3b2de46422a9a8ded72
maximusyoung007 <1091261998@qq.com>
2018-03-21 21:47:15 +0800

    chapter3

--- chapter 3 Java的基础程序设计结构/StringTest.java
+++ chapter 3 Java的基础程序设计结构/StringTest.java
@@ -2,8 +2,6 @@
 public class StringTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			// TODO Auto-generated method stub
 			String a = "Hello";
 			System.out.println(a);
 			String s = a.substring(0,3);
@@ -16,9 +14,16 @@ public class StringTest {
 				int x = 3;
 				System.out.println(x);
 			}
-			if(b == null) {
-				int x = 4;
-				System.out.println(x);
-			}
-	}
+			int n = a.length();
+			System.out.println(n);
+			int cpcount = a.codePointCount(0, a.length());
+			System.out.println(cpcount);
+			char first = a.charAt(0);
+			char last = a.charAt(4);
+			System.out.println(first);
+			System.out.println(last);
+			int index = a.offsetByCodePoints(0, 0);
+			int cp = a.codePointAt(index);
+			System.out.println(cp);
+		}
 }
