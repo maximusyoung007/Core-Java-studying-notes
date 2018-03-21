fcf001267731982b9aa5285f281cc58d74137712
maximusyoung007 <1091261998@qq.com>
2018-03-21 21:23:50 +0800

    chapter3

---
+++ chapter 3 Java的基础程序设计结构/StringTest.java
@@ -0,0 +1,24 @@
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			String a = "Hello";
+			System.out.println(a);
+			String s = a.substring(0,3);
+			System.out.println(s);
+			String b = "wor";
+			System.out.println(a + " " + b + "ld");
+			String h = a.substring(0,3) + "p!";
+			"helle".equals(a);
+			if(a.compareTo("Hello") == 0) {
+				int x = 3;
+				System.out.println(x);
+			}
+			if(b == null) {
+				int x = 4;
+				System.out.println(x);
+			}
+	}
+}
