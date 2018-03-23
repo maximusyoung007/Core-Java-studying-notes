5cb42e9198fa0fe798bee2828e0da7a13f6691d7
maximusyoung007 <1091261998@qq.com>
2018-03-23 19:12:07 +0800

    chapter3

---
+++ chapter 3 Java的基础程序设计结构/OutputTest.java
@@ -0,0 +1,11 @@
+
+public class OutputTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 10000.0 / 3.0;
+		System.out.println(a);
+		System.out.printf("%8.2f",a);//have a blank at first place
+	}
+
+}
