c9684c7a7d3b604e971bbe4499bf2121b06109f8
maximusyoung007 <1091261998@qq.com>
2018-05-11 22:36:18 +0800

    。

---
+++ python/1017 cars.py
@@ -0,0 +1,18 @@
+cars = ["bmw","audi","toyoda","subaru"]
+cars.sort()
+print(cars)
+cars.sort(reverse = True)
+print(cars)
+cars2 = ["bmw","audi","toyoda","subaru"]
+print("here is the original list:")
+print(cars2)
+print("\nhere is the sorted list:")
+print(sorted(cars2))
+print("\nhere is the original list again")
+print(cars2)
+cars3 = ["bmw","audi","toyoda","subaru"]
+print(cars3)
+cars3.reverse()
+print(cars3)
+print(len(cars3))
+
---
+++ python/1018 ex3-8 look at the world.py
@@ -0,0 +1,6 @@
+places = ["beijing","london","boston","chengdu","tokyo"]
+print(places)
+print(sorted(places))
+print(places)
+places.sorted(reverse = True)
+print(places)
