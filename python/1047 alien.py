4179e504eeb430981ec0c5d1646318cf9b8c5451
maximusyoung007 <1091261998@qq.com>
2018-05-22 22:56:25 +0800

    。

--- python/1047 alien.py
+++ python/1047 alien.py
@@ -1,3 +1,23 @@
 alien_0 = {'color' : 'green','points' : 5}
 print(alien_0['color'])
 print(alien_0['points'])
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+alien1 = {}
+alien1['color'] = 'red'
+alien1['points'] = 5
+alien2 = {'color':'green'}
+print("the alien is " + alien2['color'] + '.')
+alien2['color'] = 'yellow'
+print('the alien is now ' + alien2['color'] +'.')
+alien3 = {'x_position' : 0,'y_position' : 25,'speed' : 'medium'}
+print("Orignal x-position:" + str(alien3['x_position']))
+if alien3['speed'] == 'slow':
+	x_increment = 1;
+elif alien3['speed'] == 'medium':
+	x_increment = 2;
+else:
+	x_increment = 3;
+alien3['x_position'] = alien3['x_position'] + x_increment
+print("new x-position:" + str(alien3['x_position']))
