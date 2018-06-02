6508772b7ee9788916abdf9c5d4e8ec675f1892c
maximusyoung007 <1091261998@qq.com>
2018-06-02 20:20:50 +0800

    。

--- python/car.py
+++ python/car.py
@@ -17,3 +17,20 @@ class Car():
             print("you can't roll back an odometer!")
     def increment_odometer(selfself,miles):
         self.odometer_reading += miles
+class Battery():
+    def __init__(self,battery_size = 70):
+        self.battery_size = battery_size
+    def describe_battery(self):
+        print("this car has a " + str(self.battery_size) + "-kwh battery.")
+    def get_range(self):
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "this car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+class ElectricCar(Car):
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
+        self.battery = Battery()
