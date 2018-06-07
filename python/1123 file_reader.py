6db4af94a9141cd63682fce2cdbeed832001cf95
maximusyoung007 <1091261998@qq.com>
2018-06-07 23:01:34 +0800

    。

--- python/1123 file_reader.py
+++ python/1123 file_reader.py
@@ -3,5 +3,6 @@
   #  print(contents.rstrip())
 filename = 'pi_digits.txt'
 with open(filename) as file_object:
-    for line in file_object:
-        print(line.rstrip())
+    lines = file_object.readlines()
+for line in lines:
+    print(line.rstrip())
