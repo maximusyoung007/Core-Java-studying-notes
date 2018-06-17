3b0adf9f516dab4fb54a7b26d693f03da53983d9
maximusyoung007 <1091261998@qq.com>
2018-06-17 23:52:20 +0800

    。

--- python/1150 test_name_function.py
+++ python/1150 test_name_function.py
@@ -5,4 +5,7 @@ class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         formatted_name = get_formatted_name('janis','joplin')
         self.assertEqual(formatted_name,"Janis Joplin")
+    def test_first_last_middle_name(self):
+        formatted_name = get_formatted_name('wolfgang','mozart','amadeus')
+        self.assertEqual(formatted_name,'wolfgang Amadeus Mozart')
 unittest.main()
