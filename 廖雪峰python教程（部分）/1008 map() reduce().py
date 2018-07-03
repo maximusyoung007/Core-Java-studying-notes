a98d0dbc4a2211b40b729830820b64648832ddfa
maximusyoung007 <1091261998@qq.com>
2018-07-03 23:21:42 +0800

    python

--- 廖雪峰python教程（部分）/1008 map() reduce().py
+++ 廖雪峰python教程（部分）/1008 map() reduce().py
@@ -28,3 +28,20 @@ def char2num(s):
     return DIGITS[s]
 def str2int(s):
     return reduce(lambda x,y:x*10 + y,map(char2num,s))
+def es(s):
+    return [s[0].upper() + s[1:].lower()]
+L1 = ['admin','LISA','barT']
+L2 = list(map(es,L1))
+print(L2)
+def prod(L):
+    def fac(x,y):
+        return x * y
+    return reduce(fac,L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+def str2float(s):
+    DIGITS = {'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+
