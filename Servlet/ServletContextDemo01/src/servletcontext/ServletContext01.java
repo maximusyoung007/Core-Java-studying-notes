ae97cf52c142c2794e2b9f6212a120714332327e
maximusyoung007 <1091261998@qq.com>
2019-03-08 23:41:21 +0800

    Servlet

---
+++ Servlet/ServletContextDemo01/src/servletcontext/ServletContext01.java
@@ -0,0 +1,25 @@
+package servletcontext;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletContext01 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//获取对象
+		ServletContext context = getServletContext();
+		String address = context.getInitParameter("address");
+		System.out.println("address01:" + address);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
---
+++ Servlet/ServletContextDemo01/src/servletcontext/ServletContext02.java
@@ -0,0 +1,24 @@
+package servletcontext;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletContext02 extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext context = getServletContext();
+		String address = context.getInitParameter("address");
+		System.out.println("address02:" + address);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
